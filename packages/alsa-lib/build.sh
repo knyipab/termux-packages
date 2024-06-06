29096df9d10eb8562b094070b0aea1cbe31589f0
Ronald Y <knyipab@gmail.com>
2024-06-06 23:06:19 +0800

    alsa: attempt to fix

--- packages/alsa-lib/build.sh
+++ packages/alsa-lib/build.sh
@@ -6,6 +6,7 @@ TERMUX_PKG_VERSION="1.2.11"
 TERMUX_PKG_SRCURL="https://github.com/alsa-project/alsa-lib/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=12216f0730d6dde3ded6a2a5388bc0009ad07f5c65972bd89aac9a76f8f085a4
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-static=no"
 
 termux_step_pre_configure() {
 	autoreconf -fi
