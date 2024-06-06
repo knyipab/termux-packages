c0ba79a6f1ab150ecb214996fdf6d9f3b42a02ea
Ronald Y <knyipab@gmail.com>
2024-06-07 07:53:30 +0800

    alsa revised

--- packages/alsa-lib/build.sh
+++ packages/alsa-lib/build.sh
@@ -7,8 +7,7 @@ TERMUX_PKG_SRCURL="https://github.com/alsa-project/alsa-lib/archive/refs/tags/v$
 TERMUX_PKG_SHA256=12216f0730d6dde3ded6a2a5388bc0009ad07f5c65972bd89aac9a76f8f085a4
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---enable-shared=yes
---enable-static=no
+--disable-static
 "
 
 termux_step_pre_configure() {
