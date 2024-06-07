9cec1883d30371ee74626f9fa7f55681097d1348
Ronald Y <knyipab@gmail.com>
2024-06-07 19:15:06 +0800

    alsa-utils: aclocal_path

--- packages/alsa-utils/build.sh
+++ packages/alsa-utils/build.sh
@@ -7,7 +7,7 @@ TERMUX_PKG_SRCURL="https://github.com/alsa-project/alsa-utils/archive/refs/tags/
 TERMUX_PKG_SHA256=978961153fa8ca4c783c93767e7054d0dc1fb42ef6f1008040ca71363d0f4d35
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="ncurses"
-TERMUX_PKG_BUILD_DEPENDS="alsa-lib, alsa-lib-static"
+TERMUX_PKG_BUILD_DEPENDS="alsa-lib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-udev-rules-dir=$TERMUX_PREFIX/lib/udev/rules.d
 --with-asound-state-dir=$TERMUX_PREFIX/var/lib/alsa
@@ -19,4 +19,5 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 termux_step_pre_configure() {
 	autoreconf -fi
 	LDFLAGS+=" -llog"
+	export ACLOCAL_PATH=${TERMUX_PREFIX}/share/aclocal:$ACLOCAL_PATH
 }
