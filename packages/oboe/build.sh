bdcd8ee81b095a834a24355d99fde567c5d2cd2f
Ronald Y <knyipab@gmail.com>
2024-06-08 13:36:24 +0800

    oboe retry shared

--- packages/oboe/build.sh
+++ packages/oboe/build.sh
@@ -3,8 +3,13 @@ TERMUX_PKG_DESCRIPTION="Oboe is a C++ library that makes it easy to build high-p
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.8.1"
-# TERMUX_PKG_SRCURL="https://github.com/google/oboe/releases/download/$TERMUX_PKG_VERSION/oboe-$TERMUX_PKG_VERSION.aar"
-# TERMUX_PKG_SHA256=88e29b4fbd0ba8c60970fc785de480f6fed2aa7319c05cfaa44082f13bb8bfdf
 TERMUX_PKG_SRCURL="https://github.com/google/oboe/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=af80c16175aa4602e51f3f4378424a199e5d91476b1cba6cd00299bf1e21881f
 TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_post_make_install() {
+	TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-static"
+	termux_step_configure_autotools
+	termux_step_make
+	termux_step_make_install
+}
