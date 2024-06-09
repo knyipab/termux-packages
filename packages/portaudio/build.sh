87dc6d1abadbe187b2bf225a933b45526573f1db
Ronald Y <knyipab@gmail.com>
2024-06-09 23:05:35 +0800

    portaudio retry

--- packages/portaudio/build.sh
+++ packages/portaudio/build.sh
@@ -9,9 +9,12 @@ _DATE=20210406
 TERMUX_PKG_SRCURL=https://files.portaudio.com/archives/pa_stable_v${TERMUX_PKG_VERSION//./}_${_DATE}.tgz
 TERMUX_PKG_SHA256=47efbf42c77c19a05d22e627d42873e991ec0c1357219c0d74ce6a2948cb2def
 TERMUX_PKG_AUTO_UPDATE=false
-TERMUX_PKG_DEPENDS="libc++, pipewire, alsa-lib"
+TERMUX_PKG_DEPENDS="libc++"
+TERMUX_PKG_BUILD_DEPENDS="pulseaudio"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-cxx
+--without-alsa
+--without-jack
 --without-oss
 --without-asihpi
 ac_cv_lib_pthread_pthread_create=yes
