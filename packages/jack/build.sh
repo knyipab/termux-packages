6f1b4dc5bb2829c4939bf31124f3b6ecff495837
Ronald Y <knyipab@gmail.com>
2024-06-10 00:59:50 +0800

    jack revert

--- packages/jack/build.sh
+++ packages/jack/build.sh
@@ -1,13 +1,12 @@
 TERMUX_PKG_HOMEPAGE=https://jackaudio.org/
-TERMUX_PKG_DESCRIPTION="jack1 (portaudio backend)"
+TERMUX_PKG_DESCRIPTION="jack1 (non-funcitoning)"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_LICENSE="GPL-2.0, LGPL-2.0"
-TERMUX_PKG_DEPENDS="libdb, libandroid-sysv-semaphore, libandroid-shmem, portaudio"
+TERMUX_PKG_DEPENDS="libdb, libandroid-sysv-semaphore, libandroid-shmem"
 TERMUX_PKG_VERSION=0.126.0
 TERMUX_PKG_GIT_BRANCH=$TERMUX_PKG_VERSION
 TERMUX_PKG_SRCURL=git+https://github.com/jackaudio/jack1
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-portaudio"
 
 termux_step_pre_configure() {
 	cd ${TERMUX_PKG_SRCDIR}
