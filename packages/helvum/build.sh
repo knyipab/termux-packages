188924b78dc64b835d2946c63bbd86fe79888bfc
Ronald Y <knyipab@gmail.com>
2024-06-10 00:14:29 +0800

    helvum rebuild

--- packages/helvum/build.sh
+++ packages/helvum/build.sh
@@ -5,6 +5,6 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="0.5.1"
 TERMUX_PKG_SRCURL="https://gitlab.freedesktop.org/pipewire/helvum/-/archive/${TERMUX_PKG_VERSION}/helvum-${TERMUX_PKG_VERSION}.tar.bz2"
 TERMUX_PKG_SHA256=18ecba7174bf9f5da39cdf749e6cf260bd09b6831ba2f8165b20771cd10af4e5
-TERMUX_PKG_DEPENDS="libcairo, gdk-pixbuf, glib, graphene, gtk4, hicolor-icon-theme, libadwaita, pipewire, pango"
+TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, graphene, gtk4, hicolor-icon-theme, libadwaita, libcairo, pipewire, pango"
 TERMUX_PKG_BUILD_DEPENDS="appstream-glib"
 TERMUX_PKG_AUTO_UPDATE=true
