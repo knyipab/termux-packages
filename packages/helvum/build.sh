189f702d9e13da16833e7fb96c29ce15fb3088d1
Ronald Y <knyipab@gmail.com>
2024-06-10 01:26:42 +0800

    helvum rust

--- packages/helvum/build.sh
+++ packages/helvum/build.sh
@@ -8,3 +8,7 @@ TERMUX_PKG_SHA256=d4f5cc0c3a70a91edfc816f12a10426dadd9ca74ea82662e2df5e6c4eb31d8
 TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, graphene, gtk4, hicolor-icon-theme, libadwaita, libcairo, pipewire, pango"
 TERMUX_PKG_BUILD_DEPENDS="appstream-glib"
 TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_pre_configure() {
+	termux_setup_rust
+}
