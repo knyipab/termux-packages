a1495eaf1859ad5164d58a3187c7cf4c3e3b3763
Ronald Y <knyipab@gmail.com>
2024-06-10 14:30:51 +0800

    qpwgraph without alsa

--- packages/qpwgraph/build.sh
+++ packages/qpwgraph/build.sh
@@ -5,6 +5,6 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=0.7.2
 TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/rncbc/qpwgraph/-/archive/v$TERMUX_PKG_VERSION/qpwgraph-v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=166f5f6cd87c082b65092fb9d3aa7ab16938acc78c610b36a16adc61ac8295a0
-TERMUX_PKG_DEPENDS="hicolor-icon-theme, qt6-qtbase, qt6-qtsvg, pipewire, alsa-lib"
+TERMUX_PKG_DEPENDS="hicolor-icon-theme, qt6-qtbase, qt6-qtsvg, pipewire"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DCONFIG_ALSA_MIDI=OFF"
