5a086eeeda9e8d5edc85c061ec863beb9ecb1b8d
Ronald Y <knyipab@gmail.com>
2024-06-10 14:09:53 +0800

    coppwr attempt

---
+++ packages/coppwr/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE=https://dimtpap.ovh/coppwr
+TERMUX_PKG_DESCRIPTION="Low level control GUI for the PipeWire multimedia server"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_VERSION=1.6.0
+TERMUX_PKG_SRCURL=https://github.com/dimtpap/coppwr/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=b6ed84f3313a0a0a76d0303605d1ed55aa4ea94adb713f810680c7bde0637a8e
+TERMUX_PKG_DEPENDS="pipewire"
+TERMUX_PKG_AUTO_UPDATE=true
+
+termux_step_pre_configure() {
+	termux_setup_rust
+}
