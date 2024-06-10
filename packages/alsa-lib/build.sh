f6ae5d8ca1c14fc83a3287a28cd8718d4232b7ea
Ronald Y <knyipab@gmail.com>
2024-06-10 18:38:18 +0800

    alsa-lib without shmem

--- packages/alsa-lib/build.sh
+++ packages/alsa-lib/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.2.11"
 TERMUX_PKG_SRCURL="https://github.com/alsa-project/alsa-lib/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=12216f0730d6dde3ded6a2a5388bc0009ad07f5c65972bd89aac9a76f8f085a4
-TERMUX_PKG_DEPENDS="libandroid-sysv-semaphore, libandroid-shmem"
+TERMUX_PKG_DEPENDS="libandroid-sysv-semaphore"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-static
@@ -14,6 +14,6 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 
 termux_step_pre_configure() {
 	CPPFLAGS+=" -Wno-implicit-function-declaration"
-	LDFLAGS+=" -landroid-sysv-semaphore -landroid-shmem"
+	LDFLAGS+=" -landroid-sysv-semaphore"
 	autoreconf -fi
 }
