5f5e342e4807efaf05e807107b2780706eda1319
Ronald Y <knyipab@gmail.com>
2024-06-10 10:55:29 +0800

    helvum retry

--- packages/helvum/build.sh
+++ packages/helvum/build.sh
@@ -9,26 +9,8 @@ TERMUX_PKG_DEPENDS="gdk-pixbuf, glib, graphene, gtk4, hicolor-icon-theme, libadw
 TERMUX_PKG_BUILD_DEPENDS="appstream-glib"
 TERMUX_PKG_AUTO_UPDATE=true
 
-termux_step_configure() {
-	termux_setup_meson
+termux_step_pre_configure() {
+	sed -i "s/'-Werror=strict-prototypes',//" ${TERMUX_PKG_SRCDIR}/meson.build
+	CFLAGS+=" -Dindex=strchr -Drindex=strrchr"
 	termux_setup_rust
-
-	local _meson_buildtype="minsize"
-	local _meson_stripflag="--strip"
-	if [ "$TERMUX_DEBUG_BUILD" = "true" ]; then
-		_meson_buildtype="debug"
-		_meson_stripflag=
-	fi
-
-	CC=gcc CXX=g++ CFLAGS= CXXFLAGS= CPPFLAGS= LDFLAGS= $TERMUX_MESON \
-		setup \
-		$TERMUX_PKG_SRCDIR \
-		$TERMUX_PKG_BUILDDIR \
-		--$(test "${TERMUX_PKG_MESON_NATIVE}" = "true" && echo "native-file" || echo "cross-file") $TERMUX_MESON_CROSSFILE \
-		--prefix $TERMUX_PREFIX \
-		--libdir lib \
-		--buildtype ${_meson_buildtype} \
-		${_meson_stripflag} \
-		$TERMUX_PKG_EXTRA_CONFIGURE_ARGS \
-		|| (termux_step_configure_meson_failure_hook && false)
 }
