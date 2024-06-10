fda2080bf0f810d5347f14a4291fe5e3bbf1ee15
Ronald Y <knyipab@gmail.com>
2024-06-10 17:32:14 +0800

    pipewire redo

--- packages/alsa-lib/build.sh
+++ packages/alsa-lib/build.sh
@@ -7,13 +7,6 @@ TERMUX_PKG_SRCURL="https://github.com/alsa-project/alsa-lib/archive/refs/tags/v$
 TERMUX_PKG_SHA256=12216f0730d6dde3ded6a2a5388bc0009ad07f5c65972bd89aac9a76f8f085a4
 TERMUX_PKG_DEPENDS="libandroid-shmem"
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
---disable-static
---disable-pcm
---disable-ucm
---disable-topology
---with-versioned=no
-"
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-shmem"
