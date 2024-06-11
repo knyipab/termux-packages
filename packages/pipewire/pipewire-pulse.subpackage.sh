cc79c5d1160692bd1a428ef560aa9f6334b8cb7a
Ronald Y <knyipab@gmail.com>
2024-06-12 07:07:38 +0800

    pipewire retry

--- packages/pipewire/pipewire-pulse.subpackage.sh
+++ packages/pipewire/pipewire-pulse.subpackage.sh
@@ -1,9 +1,8 @@
-TERMUX_SUBPKG_DESCRIPTION="pipewire - ALSA configuration"
-$_so_ver=0.3
+TERMUX_SUBPKG_DESCRIPTION="pipewire - ALSA configuration"S
 TERMUX_SUBPKG_INCLUDE="
 bin/pipewire-pulse
 etc/pipewire/pipewire-pulse.conf.d
-lib/pipewire-$_so_ver/libpipewire-module-protocol-pulse.so
-lib/pipewire-$_so_ver/libpipewire-module-pulse-tunnel.so
+lib/pipewire-*/libpipewire-module-protocol-pulse.so
+lib/pipewire-*/libpipewire-module-pulse-tunnel.so
 share/pipewire/pipewire-pulse.conf*
 "
