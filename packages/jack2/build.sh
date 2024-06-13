ea5ab66b69482fe7616be3ffc5a091503e580442
Ronald Y <knyipab@gmail.com>
2024-06-13 23:56:25 +0800

    jack2 fix semaphore

--- packages/jack2/build.sh
+++ packages/jack2/build.sh
@@ -5,12 +5,12 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1.9.22
 TERMUX_PKG_SRCURL=https://github.com/jackaudio/jack2/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=1e42b9fc4ad7db7befd414d45ab2f8a159c0b30fcd6eee452be662298766a849
-TERMUX_PKG_DEPENDS="alsa-lib, libdb, dbus, libandroid-shmem, libandroid-posix-semaphore, libandroid-sysv-semaphore, libexpat, libsamplerate, libopus"
+TERMUX_PKG_DEPENDS="alsa-lib, libdb, dbus, libandroid-shmem, libandroid-posix-semaphore, libexpat, libsamplerate, libopus"
 TERMUX_PKG_CONFLICTS="jack1, pipewire-jack"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_configure() {
-	LDFLAGS+=" -landroid-shmem -landroid-posix-semaphore -landroid-sysv-semaphore"
+	LDFLAGS+=" -landroid-shmem -landroid-posix-semaphore"
 	python3 ./waf configure --alsa --classic --prefix="$TERMUX_PREFIX" --htmldir=$TERMUX_PREFIX/share/doc/jack2/html --firewire=no
 }
 
