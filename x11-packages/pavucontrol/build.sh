737875c6120c4e6b275a0c44b40b9080401c856c
Ronald Y <knyipab@gmail.com>
2024-06-14 23:46:51 +0800

    pavucontrol retry

--- x11-packages/pavucontrol/build.sh
+++ x11-packages/pavucontrol/build.sh
@@ -5,5 +5,5 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=6.0
 TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/pulseaudio/pavucontrol/-/archive/v$TERMUX_PKG_VERSION/pavucontrol-v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=0d7ced3b9d215bcfae8fd5df5429aa07b0517984925cfa1cd020a9505749a994
-TERMUX_PKG_DEPENDS="gtk4, gtkmm4, json-glib, libcanberra, pulseaudio, libsigc++-3.0"
+TERMUX_PKG_DEPENDS="gtk4, gtkmm4, json-glib, libcanberra, libsigc++-3.0, pulseaudio"
 TERMUX_PKG_AUTO_UPDATE=true
