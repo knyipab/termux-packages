3775b0480d4c2c0cf47f75269c14eaf223f91e59
Ronald Y <knyipab@gmail.com>
2024-06-14 23:46:02 +0800

    qjackctl qt5

--- packages/qjackctl/build.sh
+++ packages/qjackctl/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=0.9.91
 TERMUX_PKG_SRCURL=git+https://github.com/rncbc/qjackctl
 TERMUX_PKG_GIT_BRANCH=qjackctl_${TERMUX_PKG_VERSION//./_}
-TERMUX_PKG_DEPENDS="hicolor-icon-theme, qt6-qtbase, qt6-qtsvg, qt6-qttools, qt6-qttranslations, alsa-lib, jack"
-TERMUX_PKG_BUILD_DEPENDS="qt6-qtbase-cross-tools, jack2"
+TERMUX_PKG_DEPENDS="hicolor-icon-theme, qt5-qtbase, qt5-qtsvg, qt5-qttools, alsa-lib, jack"
+TERMUX_PKG_BUILD_DEPENDS="qt5-qtbase-cross-tools, jack2"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DCONFIG_SYSTEM_TRAY=no -DCONFIG_PORTAUDIO=no"
