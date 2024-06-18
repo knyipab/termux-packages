bb22e8cd037e0a0ba7ece6af05a3843be8914371
Ronald Y <46513942+knyipab@users.noreply.github.com>
2024-06-19 00:45:48 +0800

    Update build.sh

--- packages/qjackctl/build.sh
+++ packages/qjackctl/build.sh
@@ -8,5 +8,5 @@ TERMUX_PKG_GIT_BRANCH=qjackctl_${TERMUX_PKG_VERSION//./_}
 TERMUX_PKG_DEPENDS="alsa-lib, hicolor-icon-theme, jack, qt6-qtbase, qt6-qtsvg, qt6-qttools"
 TERMUX_PKG_BUILD_DEPENDS="qt6-qtbase-cross-tools, jack2"
 TERMUX_PKG_AUTO_UPDATE=true
-# TODO: remove below line when the libandroid-sysv-semaphore for Android 14+ is implemented (https://github.com/termux/termux-packages/issues/20571)
+# TODO: remove below line when QSharedMemoryPosix leakage issue is fixed by implementing the libandroid-sysv-semaphore for Android 14+ (https://github.com/termux/termux-packages/issues/20571)
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DCONFIG_SYSTEM_TRAY=no -DCONFIG_XUNIQUE=no"
